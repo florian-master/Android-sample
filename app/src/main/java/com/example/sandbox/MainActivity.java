489680c2634cdb85c490cec859cd11829f4b1789
Florian <tkingflorian@live.be>
2021-02-19 11:58:49 +0100

    communication with API enabled

--- app/src/main/java/com/example/sandbox/MainActivity.java
+++ app/src/main/java/com/example/sandbox/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.sandbox;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    TextView textView;
+    final String baseURL = "https://prevision-meteo.ch/services/json/bordeaux";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.text);
+
+
+        textView = findViewById(R.id.textView);
+        Button buttonView = findViewById(R.id.button);
+        buttonView.setOnClickListener(callApi);
 
         JoystickView joystickView1 = findViewById(R.id.joystick1);
         JoystickView joystickView2 = findViewById(R.id.joystick2);
@@ -31,6 +50,38 @@ public class MainActivity extends WearableActivity {
         // Enables Always-on
         setAmbientEnabled();
     }
+
+    private View.OnClickListener callApi = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            RequestQueue req = Volley.newRequestQueue(getApplicationContext());
+            JsonObjectRequest objectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    baseURL,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            textView.setText("SUCCESS");
+                            try {
+                                Log.i("response", response.get("city_info").toString());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            textView.setText("FAILURE");
+                            Log.e("respErr", error.toString());
+                        }
+                    }
+            );
+            req.add(objectRequest);
+        }
+    };
 
     @Override
     protected void onStart() {
