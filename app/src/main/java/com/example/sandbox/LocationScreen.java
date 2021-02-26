b25ea4558812fb33a1bda5a6c2ab3cc2e2cba952
Florian <tkingflorian@live.be>
2021-02-26 02:13:31 +0100

    collect location data

--- app/src/main/java/com/example/sandbox/LocationScreen.java
+++ app/src/main/java/com/example/sandbox/LocationScreen.java
@@ -1,16 +1,42 @@
 package com.example.sandbox;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+
 public class LocationScreen extends Fragment {
     private TextView lng;
     private TextView lat;
+    private LocationRequest request;
+    private final int REQUEST_CODE = 8990;
+    private LocationCallback locationCallback;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
     public LocationScreen() {
         // Required empty public constructor
     }
@@ -20,6 +46,82 @@ public class LocationScreen extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.location, container, false);
+
+        lng = rootView.findViewById(R.id.longitude);
+        lat = rootView.findViewById(R.id.latitude);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        request = new LocationRequest()
+                .setFastestInterval(300)
+                .setInterval(300)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(request);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnFailureListener(getActivity(), new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    ResolvableApiException resolvable = (ResolvableApiException) e;
+                    try {
+                        resolvable.startResolutionForResult(getActivity(), REQUEST_CODE);
+                    } catch (IntentSender.SendIntentException sendIntentException) {
+                        sendIntentException.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    lng.setText("null");
+                    return;
+                }
+
+                for (Location location : locationResult.getLocations()) {
+                    lng.setText(String.valueOf(location.getLongitude()));
+                    lat.setText(String.valueOf(location.getLatitude()));
+                }
+            }
+        };
+
         return rootView;
+    }
+
+    private void startLocationUpdates() {
+        if (fusedLocationProviderClient != null) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                Log.e("permission denied", "use location");
+                return;
+            }
+            fusedLocationProviderClient.requestLocationUpdates(request, locationCallback, Looper.getMainLooper());
+        }
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onPause() {
+
+        super.onPause();
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
     }
 }
